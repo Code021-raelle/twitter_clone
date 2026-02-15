190b5093cafc61ef1762a84a0a1077fbe79f0952
Gabriel Akinshola <gabrielakinshola3@gmail.com>
2026-02-15 17:24:34 +0100

    push notificati on

--- x_clone/app/utils/notifications.py
+++ x_clone/app/utils/notifications.py
@@ -1,6 +1,7 @@
 from app.models.notification import Notification
+from app.websockets.notifications import manager
 
-def create_notification(db, recipient_id, actor_id, type, tweet_id=None):
+async def create_notification(db, recipient_id, actor_id, type, tweet_id=None):
     notification = Notification(
         recipient_id=recipient_id,
         actor_id=actor_id,
@@ -10,4 +11,11 @@ def create_notification(db, recipient_id, actor_id, type, tweet_id=None):
     db.add(notification)
     db.commit()
     db.refresh(notification)
-    return notification
+    
+    await manager.send(recipient_id, {
+        "id": notification.id,
+        "actor_id": actor_id,
+        "type": type,
+        "tweet_id": tweet_id,
+        "created_at": str(notification.created_at)
+    })
