90c6e5f0097499f45dee9bbf4067f950c7073ed4
Gabriel Akinshola <gabrielakinshola3@gmail.com>
2026-02-15 10:32:40 +0100

    added handle click for notifications

--- frontend/src/pages/Notifications.jsx
+++ frontend/src/pages/Notifications.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import API from "../services/api";
 import Layout from "../components/Layout";
 
 export default function Notifications() {
     const [notifications, setNotifications] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         fetchNotifications();
@@ -13,6 +15,16 @@ export default function Notifications() {
         const res = await API.get("/notifications");
         setNotifications(res.data);
         await API.post("/notifications/read");
+    };
+
+    const handleClick = (n) => {
+        if (n.type === "follow") {
+            navigate(`/users/${n.actor_id}`);
+        }
+
+        if (n.type === "like" || n.type === "retweet") {
+            navigate(`/tweets/${n.tweet_id}`);
+        }
     };
 
     const message = (n) => {
@@ -29,12 +41,13 @@ export default function Notifications() {
                 {notifications.map((n) => (
                     <div
                         key={n.id}
-                        className={`p-3 border-b ${
+                        onClick={() => handleClick(n)}
+                        className={`p-3 border-b cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-900 ${
                             n.is_read ? "opacity-70" : ""
                         }`}
                     >
                         <p>{message(n)}</p>
-                        <span className="text=xs text-gray-500">
+                        <span className="text-xs text-gray-500">
                             {new Date(n.created_at).toLocaleString()}
                         </span>
                     </div>
